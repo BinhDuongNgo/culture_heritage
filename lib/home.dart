fd773d87f931b8b368ac83a7497c672bab04014a
Ngo Binh Duong <binhduong772@gmail.com>
2024-05-21 01:08:00 +0700

    feat: update back button

--- lib/home.dart
+++ lib/home.dart
@@ -5,9 +5,9 @@ import 'package:travel_application/pages/booking_page.dart';
 import 'package:travel_application/pages/home_page.dart';
 // ignore: depend_on_referenced_packages
 import 'package:google_nav_bar/google_nav_bar.dart';
+import 'package:travel_application/pages/notification_page.dart';
 import 'package:travel_application/pages/profile_page.dart';
 import 'package:travel_application/pages/search_page.dart';
-import 'package:travel_application/pages/settings_page.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -24,53 +24,64 @@ class _Home extends State<Home> {
   ];
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Color.fromRGBO(224, 224, 224, 1.0),
-        leading: Icon(Icons.notifications_active),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.settings),
-            onPressed: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => SettingsPage()));
-                  //SettingPage.dart
-            },
+    return WillPopScope(
+      onWillPop: () async {
+        return false;
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Color.fromRGBO(224, 224, 224, 1.0),
+          automaticallyImplyLeading: false,
+          actions: [
+            IconButton(
+              icon: Icon(Icons.notifications),
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => NotificationPage()));
+                //SettingPage.dart
+              },
+            ),
+          ],
+        ),
+        body: Container(
+          child: _widgetOptions.elementAt(_selectedIndex),
+        ),
+        bottomNavigationBar: Container(
+          color: Colors.grey.shade300,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 15),
+            child: GNav(
+              backgroundColor: Colors.grey.shade300,
+              tabBackgroundColor: Colors.white,
+              padding: EdgeInsets.all(10),
+              gap: 8,
+              selectedIndex: _selectedIndex,
+              onTabChange: (index) {
+                setState(() {
+                  _selectedIndex = index;
+                });
+              },
+              tabs: [
+                GButton(
+                  icon: Icons.home_outlined,
+                  text: 'Home',
+                ),
+                GButton(
+                  icon: Icons.bookmark_outline,
+                  text: 'Booking',
+                ),
+                GButton(
+                  icon: Icons.search,
+                  text: 'Search',
+                ),
+                GButton(
+                  icon: Icons.person_outline,
+                  text: 'Profile',
+                ),
+              ],
+            ),
           ),
-        ],
-      ),
-      body: Container(
-        child: _widgetOptions.elementAt(_selectedIndex),
-      ),
-      bottomNavigationBar: GNav(
-        backgroundColor: Color.fromRGBO(224, 224, 224, 1.0),
-        tabBackgroundColor: Colors.white,
-        padding: EdgeInsets.all(16),
-        gap: 10,
-        selectedIndex: _selectedIndex,
-        onTabChange: (index) {
-          setState(() {
-            _selectedIndex = index;
-          });
-        },
-        tabs: [
-          GButton(
-            icon: Icons.home_outlined,
-            text: 'Home',
-          ),
-          GButton(
-            icon: Icons.bookmark_outline,
-            text: 'Booking',
-          ),
-          GButton(
-            icon: Icons.search,
-            text: 'Search',
-          ),
-          GButton(
-            icon: Icons.person_outline,
-            text: 'Profile',
-          ),
-        ],
+        ),
       ),
     );
   }

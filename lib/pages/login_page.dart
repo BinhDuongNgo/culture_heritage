60ebff1ada61a6741dbfc1c99750f59cd6329a2f
Ngo Binh Duong <binhduong772@gmail.com>
2024-05-21 22:14:21 +0700

    style: update responsive UI and format code

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,7 +1,4 @@
-// ignore_for_file: prefer_const_constructors
-import 'dart:ui';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:travel_application/home.dart';
 import 'package:travel_application/pages/register_page.dart';
 
@@ -10,228 +7,220 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
       backgroundColor: Color.fromRGBO(224, 224, 224, 1.0),
       body: Column(
-        // ignore: prefer_const_literals_to_create_immutables
         children: [
           SizedBox(
-            height: 250,
+            width: double.maxFinite,
+            height: screenHeight * 0.25,
             child: Center(
               child: Image(
                 image: AssetImage('assets/images/logo.png'),
               ),
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.only(left: 25, bottom: 5, top: 10),
-            child: Container(
-              width: double.infinity,
-              height: 20,
-              child: Text(
-                "Email",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 25.0),
-            child: TextField(
-              decoration: InputDecoration(
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.white),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.grey.shade400),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                fillColor: Colors.white,
-                filled: true,
-                hintText: "example@gmail.com",
-                hintStyle: TextStyle(
-                  color: Colors.grey,
-                ),
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 25, bottom: 5, top: 10),
-            child: Container(
-              width: double.infinity,
-              height: 20,
-              child: Text(
-                "Password",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 25.0),
-            child: TextField(
-              decoration: InputDecoration(
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.white),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.grey.shade400),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                fillColor: Colors.white,
-                filled: true,
-                hintText: "password",
-                hintStyle: TextStyle(
-                  color: Colors.grey,
-                ),
-              ),
-            ),
-          ),
           SizedBox(
-            height: 40,
-          ),
-          InkWell(
-            onTap: () {
-              Navigator.push(
-                  context, MaterialPageRoute(builder: (context) => Home()));
-            },
-            child: Container(
-              padding: EdgeInsets.all(20),
-              margin: EdgeInsets.symmetric(horizontal: 25),
-              decoration: BoxDecoration(
-                color: Colors.orange.shade300,
-                borderRadius: BorderRadius.circular(25),
-              ),
-              child: Center(
-                child: Text(
-                  "Sign in",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
+            width: double.maxFinite,
+            height: screenHeight * 0.3,
+            child: Column(
+              children: [
+                SizedBox(
+                  height: screenHeight * 0.10,
+                  width: screenWidth * 0.85,
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      label: Text("Email"),
+                      prefixIcon: Icon(Icons.person),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(25),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(25),
+                        borderSide: BorderSide(
+                          width: 2,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
                   ),
                 ),
-              ),
+                SizedBox(
+                  height: screenHeight * 0.10,
+                  width: screenWidth * 0.85,
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      label: Text("Password"),
+                      prefixIcon: Icon(Icons.lock_person),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(25),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(25),
+                        borderSide: BorderSide(
+                          width: 2,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ),
-          SizedBox(
-            height: 15,
-          ),
-          Center(
-            child: Text("Or"),
-          ),
-          SizedBox(
-            height: 15,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                padding: EdgeInsets.all(20),
-                margin: EdgeInsets.symmetric(horizontal: 15),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(25),
-                ),
-                child: Row(
-                  children: [
-                    Icon(
-                      Icons.g_mobiledata,
-                      color: Colors.orange,
-                    ),
-                    SizedBox(
-                      width: 10,
-                    ),
-                    Center(
-                      child: Text(
-                        "Google",
-                        style: TextStyle(
-                          color: Colors.grey.shade800,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.all(20),
-                margin: EdgeInsets.symmetric(horizontal: 15),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(25),
-                ),
-                child: Row(
-                  children: [
-                    Icon(
-                      Icons.facebook,
-                      color: Colors.blue,
-                    ),
-                    SizedBox(
-                      width: 10,
-                    ),
-                    Center(
-                      child: Text(
-                        "Facebook",
-                        style: TextStyle(
-                          color: Colors.grey.shade800,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-          SizedBox(
-            height: 10,
           ),
           SizedBox(
             width: double.maxFinite,
-            height: 50,
-            child: Container(
-              child: Center(
-                child: Row(
+            height: screenHeight * 0.4,
+            child: Column(
+              children: [
+                InkWell(
+                  onTap: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => Home()));
+                  },
+                  child: Container(
+                    padding: EdgeInsets.all(20),
+                    margin: EdgeInsets.symmetric(horizontal: 25),
+                    decoration: BoxDecoration(
+                      color: Colors.orange.shade300,
+                      borderRadius: BorderRadius.circular(25),
+                    ),
+                    child: Center(
+                      child: Text(
+                        "Sign in",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: screenHeight * 0.015,
+                ),
+                Center(
+                  child: Text("Or"),
+                ),
+                SizedBox(
+                  height: screenHeight * 0.015,
+                ),
+                Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Text(
-                      "Don't have an account? ",
-                    ),
-                    InkWell(
-                      child: Text(
-                        "Sign up",
-                        style: TextStyle(color: Colors.blue),
+                    Container(
+                      padding: EdgeInsets.all(20),
+                      margin: EdgeInsets.symmetric(horizontal: 15),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(25),
                       ),
-                      onTap: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => RegisterPage()));
-                      },
+                      child: Row(
+                        children: [
+                          Icon(
+                            Icons.g_mobiledata,
+                            color: Colors.orange,
+                          ),
+                          SizedBox(
+                            width: screenWidth * 0.01,
+                          ),
+                          Center(
+                            child: Text(
+                              "Google",
+                              style: TextStyle(
+                                color: Colors.grey.shade800,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.all(20),
+                      margin: EdgeInsets.symmetric(horizontal: 15),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(25),
+                      ),
+                      child: Row(
+                        children: [
+                          Icon(
+                            Icons.facebook,
+                            color: Colors.blue,
+                          ),
+                          SizedBox(
+                            width: screenWidth * 0.01,
+                          ),
+                          Center(
+                            child: Text(
+                              "Facebook",
+                              style: TextStyle(
+                                color: Colors.grey.shade800,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ],
                 ),
-              ),
+                SizedBox(
+                  height: screenHeight * 0.01,
+                ),
+                SizedBox(
+                  width: double.maxFinite,
+                  height: screenHeight * 0.05,
+                  child: Container(
+                    child: Center(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "Don't have an account? ",
+                          ),
+                          InkWell(
+                            child: Text(
+                              "Sign up",
+                              style: TextStyle(color: Colors.blue),
+                            ),
+                            onTap: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => RegisterPage()));
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
           SizedBox(
-            height: 100,
-          ),
-          Center(
-              child: InkWell(
-            onTap: () {
-              Navigator.push(
-                  context, MaterialPageRoute(builder: (context) => Home()));
-            },
-            child: Text(
-              "Explore the app as a guest",
-              style: TextStyle(
-                color: Colors.grey.shade600,
-              ),
+            width: double.maxFinite,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Center(
+                    child: InkWell(
+                  onTap: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => Home()));
+                  },
+                  child: Text(
+                    "Explore the app as a guest",
+                    style: TextStyle(
+                      color: Colors.grey.shade600,
+                    ),
+                  ),
+                )),
+              ],
             ),
-          )),
+          ),
         ],
       ),
     );
--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -1,7 +1,4 @@
-// ignore_for_file: prefer_const_constructors
-import 'dart:ui';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:travel_application/pages/login_page.dart';
 
 class RegisterPage extends StatelessWidget {
@@ -9,176 +6,155 @@ class RegisterPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
       backgroundColor: Color.fromRGBO(224, 224, 224, 1.0),
       body: Column(
-        // ignore: prefer_const_literals_to_create_immutables
         children: [
           SizedBox(
-            height: 250,
+            width: double.maxFinite,
+            height: screenHeight * 0.25,
             child: Center(
               child: Image(
                 image: AssetImage('assets/images/logo.png'),
               ),
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.only(left: 25, bottom: 5, top: 10),
-            child: Container(
-              width: double.infinity,
-              height: 20,
-              child: Text(
-                "Email",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 25.0),
-            child: TextField(
-              decoration: InputDecoration(
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.white),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.grey.shade400),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                fillColor: Colors.white,
-                filled: true,
-                hintText: "example@gmail.com",
-                hintStyle: TextStyle(
-                  color: Colors.grey,
-                ),
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 25, bottom: 5, top: 10),
-            child: Container(
-              width: double.infinity,
-              height: 20,
-              child: Text(
-                "Password",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 25.0),
-            child: TextField(
-              decoration: InputDecoration(
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.white),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.grey.shade400),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                fillColor: Colors.white,
-                filled: true,
-                hintText: "password",
-                hintStyle: TextStyle(
-                  color: Colors.grey,
-                ),
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 25, bottom: 5, top: 10),
-            child: Container(
-              width: double.infinity,
-              height: 20,
-              child: Text(
-                "Confirm password",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 25.0),
-            child: TextField(
-              decoration: InputDecoration(
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.white),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.grey.shade400),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                fillColor: Colors.white,
-                filled: true,
-                hintText: "confirm password",
-                hintStyle: TextStyle(
-                  color: Colors.grey,
-                ),
-              ),
-            ),
-          ),
           SizedBox(
-            height: 50,
-          ),
-          InkWell(
-            onTap: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => LoginPage()));
-            },
-            child: Container(
-              padding: EdgeInsets.all(20),
-              margin: EdgeInsets.symmetric(horizontal: 25),
-              decoration: BoxDecoration(
-                color: Colors.orange.shade300,
-                borderRadius: BorderRadius.circular(25),
-              ),
-              child: Center(
-                child: Text(
-                  "Sign up",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
+            width: double.maxFinite,
+            height: screenHeight * 0.40,
+            child: Column(
+              children: [
+                SizedBox(
+                  height: screenHeight * 0.10,
+                  width: screenWidth * 0.85,
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      label: Text("Email"),
+                      prefixIcon: Icon(Icons.person),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(25),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(25),
+                        borderSide: BorderSide(
+                          width: 2,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
                   ),
                 ),
-              ),
+                SizedBox(
+                  height: screenHeight * 0.10,
+                  width: screenWidth * 0.85,
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      label: Text("Password"),
+                      prefixIcon: Icon(Icons.lock_person),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(25),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(25),
+                        borderSide: BorderSide(
+                          width: 2,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: screenHeight * 0.10,
+                  width: screenWidth * 0.85,
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      label: Text("Confirm password"),
+                      prefixIcon: Icon(Icons.lock_person),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(25),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(25),
+                        borderSide: BorderSide(
+                          width: 2,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ),
-          SizedBox(
-            height: 30,
           ),
           SizedBox(
             width: double.maxFinite,
-            height: 50,
-            child: Container(
-              child: Center(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      "Already have an account? ",
+            height: screenHeight * 0.3,
+            child: Column(
+              children: [
+                InkWell(
+                  onTap: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => LoginPage()));
+                  },
+                  child: Container(
+                    padding: EdgeInsets.all(20),
+                    margin: EdgeInsets.symmetric(horizontal: 25),
+                    decoration: BoxDecoration(
+                      color: Colors.orange.shade300,
+                      borderRadius: BorderRadius.circular(25),
                     ),
-                    InkWell(
+                    child: Center(
                       child: Text(
-                        "Sign in",
-                        style: TextStyle(color: Colors.blue),
+                        "Sign up",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
-                      onTap: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => LoginPage()));
-                      },
                     ),
-                  ],
+                  ),
                 ),
-              ),
+                SizedBox(
+                  height: screenHeight * 0.015,
+                ),
+                Center(
+                  child: Text("Or"),
+                ),
+                SizedBox(
+                  height: screenHeight * 0.015,
+                ),
+                SizedBox(
+                  width: double.maxFinite,
+                  height: screenHeight * 0.05,
+                  child: Container(
+                    child: Center(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "Already have an account? ",
+                          ),
+                          InkWell(
+                            child: Text(
+                              "Sign in",
+                              style: TextStyle(color: Colors.blue),
+                            ),
+                            onTap: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => LoginPage()));
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
         ],

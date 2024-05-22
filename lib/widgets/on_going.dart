eeee0ab3a48cf0c1276c77c5d616197212798870
Ngo Binh Duong <binhduong772@gmail.com>
2024-05-22 13:21:10 +0700

    style: update responsive UI and format code

--- lib/widgets/on_going.dart
+++ lib/widgets/on_going.dart
@@ -9,7 +9,6 @@ class OnGoing extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    double screenWidth = MediaQuery.of(context).size.width;
     double screenHeight = MediaQuery.of(context).size.height;
     return SizedBox(
       height: screenHeight*1,
